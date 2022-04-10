756521f9a5151be3e4bd1bb29c50021a1250db7e
DESKTOP-CVPK38D\trido <tringuyen.nesttech@gmail.com>
2022-04-10 15:56:35 +0700

    first push

---
+++ app/src/main/java/com/torrydo/test_try_catch_ext_library/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.torrydo.test_try_catch_ext_library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
